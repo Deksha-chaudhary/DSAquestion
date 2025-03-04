fe6db2ead431391ec6dcbbf94a1596a76dfa3ae9
Deksha <dekshachaudhary91@gmail.com>
2025-03-04 15:18:54 +0530

    change commit

--- Insert_DeleteElmnt.java
+++ Insert_DeleteElmnt.java
@@ -1,22 +1,44 @@
+import java.util.ArrayList;
+
 public class Insert_DeleteElmnt {
     public static void main(String[] args) {
-        int a[]={1,2,3,4};
-        int b[]=new int[a.length+1];
-        int n=10;
-        int p=2;
-        for(int i=0;i<a.length;i++){
-            if(i!=p){
-                b[i++]=a[i];
-            }else if(i==p){
-                b[i]=n;
-            }
+        // int a[]={1,2,3,4,0};
 
-            
-
+        // int n=10;
+        // int p=2;
+        // for(int i=a.length-1;i>p;i--){
+        // a[i]=a[i-1];
+        // }
+        // a[p]=n;
+        // for(int i=0;i<a.length;i++){
+        // System.out.println(a[i]);
+        // }
+              
+                  //------- delete element----
+        int p = 2;
+        int a[] = { 1, 2, 3, 4 };
+        for(int i=p;i<a.length-1;i++){
+            a[i]=a[i+1];
         }
-        for(int i=0;i<b.length;i++){
-            System.out.println(b[i]);
-        }
-    }
     
+        for(int i=0;i<a.length-1;i++){
+            System.out.println(a[i]);
+        }
+      
+
+         //-------  insert and delete by using arraylist--------
+
+        // ArrayList<Integer> list = new ArrayList<>();
+        // int n = 10;
+        // int p = 2;
+        // int a[] = { 1, 2, 3, 4 };
+        // for (int num : a) {
+        //     list.add(num);
+        // }
+        // list.remove(p);                 // delete element
+        // list.add(p, n);                // insert element
+        // System.out.println(list);
+
+    }
+
 }
