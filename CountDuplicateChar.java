b4f679a6f8bd96428c40461b1971fe8617d29e46
Deksha <dekshachaudhary91@gmail.com>
2025-03-31 22:06:10 +0530

    save change

---
+++ CountDuplicateChar.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+
+public class CountDuplicateChar {
+    public static void main(String[] args) {
+        String str="deekshaa";
+        // isDuplicateChar(str); 
+
+        //-----using array----------
+       int a[]=new int[256];
+       for(int item:str.toCharArray()){
+        a[item]++;
+       }
+
+       for(int i=0;i<a.length;i++){
+        if(a[i]>1){
+
+System.out.println((char)i +""+a[i]);
+        }
+       }
+
+        
+    }
+
+    //----- using hashmap----------------
+//     public static  void isDuplicateChar(String s){
+        
+//         HashMap<Character,Integer> map=new HashMap<>(); 
+//         for(char c:s.toCharArray()){
+//             map.put(c,map.getOrDefault(c,0)+1);
+            
+            
+//     }
+//     for(char ch:map.keySet()){
+// if(map.get(ch)>1){
+//     System.out.println(ch+ " "+map.get(ch));
+// }
+//     }
+    
+// }
+}
