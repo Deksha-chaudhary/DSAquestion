2b0787f80b32775fc82d35d945411fb90efac55b
Deksha <dekshachaudhary91@gmail.com>
2025-03-31 22:16:45 +0530

    save change

--- CountDuplicateChar.java
+++ CountDuplicateChar.java
@@ -1,40 +1,39 @@
-import java.util.HashMap;
+import java.util.*;
 
 public class CountDuplicateChar {
     public static void main(String[] args) {
         String str="deekshaa";
-        // isDuplicateChar(str); 
+        isDuplicateChar(str); 
 
         //-----using array----------
-       int a[]=new int[256];
-       for(int item:str.toCharArray()){
-        a[item]++;
-       }
+//        int a[]=new int[256];
+//        for(int item:str.toCharArray()){
+//         a[item]++;
+//        }
 
-       for(int i=0;i<a.length;i++){
-        if(a[i]>1){
+//        for(int i=0;i<a.length;i++){
+//         if(a[i]>1){
 
-System.out.println((char)i +""+a[i]);
-        }
-       }
+// System.out.println((char)i +""+a[i]);
+//         }
+//        }
 
         
     }
 
-    //----- using hashmap----------------
-//     public static  void isDuplicateChar(String s){
+    // ----- using hashmap also sort ----------------
+    public static  void isDuplicateChar(String s){
         
-//         HashMap<Character,Integer> map=new HashMap<>(); 
-//         for(char c:s.toCharArray()){
-//             map.put(c,map.getOrDefault(c,0)+1);
-            
-            
-//     }
-//     for(char ch:map.keySet()){
-// if(map.get(ch)>1){
-//     System.out.println(ch+ " "+map.get(ch));
-// }
-//     }
+        Map<Character,Integer> map=new TreeMap(); 
+        for(char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+            }
+            System.out.println(map);
+    for(char ch:map.keySet()){
+if(map.get(ch)>1){
+    System.out.println(ch+ " "+map.get(ch));
+}
+    }
     
-// }
+}
 }
