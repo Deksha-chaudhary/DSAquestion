a1470ccca5476352c00caff424b285664adb2acc
Deksha <dekshachaudhary91@gmail.com>
2025-05-09 15:40:49 +0530

    Initial commit

---
+++ IsArraySort2.java
@@ -0,0 +1,15 @@
+public class IsArraySort2 {
+    public static void main(String[] args) {
+        int a[]={9,8,7,6,5,4,3};
+        System.out.println(isSort(a));
+        
+    }
+    static boolean isSort(int a[]){
+for(int i=1;i<a.length;i++){
+if(a[i]>a[i-1]){
+    return false;
+}
+}
+return true;
+    }
+}
